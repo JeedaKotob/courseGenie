02fc082d5dcf2c6970a468a809d540a6841d7368
Jeeda2004 <jk8835@g.rit.edu>
2025-09-20 11:33:07 +0400

    New courseGenie sample data

---
+++ courseGenie_backEnd/queries/courseGenie.sql
@@ -0,0 +1,177 @@
+INSERT INTO course_genie.professor
+  (professor_id, first_name, last_name, user_name, email, office, phone, office_hours)
+VALUES
+  (1,'Ali','Assi','axacad5','axacad5@rit.edu','D002','2060','Mon.: 11- 12 and 1 - 2:30, Wed.: 1 - 2-30'),
+  (2,'Ahmed','Mostafa','axmcad4','axmcad4@rit.edu','D003','2070','Mon.: 11- 12 and 1 - 2:30, Wed.: 1 - 2-30'),
+  (3,'Huda','Saadeh','hkscad','hkscad@rit.edu','D206','2150','Tu., Thu. 12:00-13:00 Tu., 13:00-14:00 Mon. 12:00-13:00'),
+  (4,'Qusai','Hasan','qxhcad','qxhcad@rit.edu','D207','2170','Tu., Thu. 12:00-13:00 Tu., 13:00-14:00 Mon. 12:00-13:00'),
+  (5,'Mohamed','Abdelraheem','mmtcad1','mmtcad1@rit.edu','D303','3030','Monday from [10:00 AM to 12:00PM] Wednesday from [9:30 AM to 10:30AM] Thursday from [9:30 AM to 10:300 AM]'),
+  (6,'Khalil','Al Hussaeni','kxacad','kxacad@rit.edu','D311','3712','TueThu: 12:00pm - 1:30pm'),
+  (7,'Zainab','Al Zanbouri','zxacad','zxacad@rit.edu','D005','3050','Tuesday 10:30 am to 12 pm - Thursday 10:30 am to 12 pm - Wednesday 1:30 pm to 2:30 pm'),
+  (8,'Mahmood','Albashayreh','mgacad','mgacad@rit.edu','Online','00962797120018','Schedule via Starfish Platform'),
+  (9,'Omar','Abdul Latif','omacad','omacad@rit.edu','D106','043712052','Schedule via Starfish Platform'),
+  (10,'Kevser','Akpinar','kxocad1','kxocad1@rit.edu','D208','2080','MonWed: 12:00pm - 1:30pm'),
+  (11,'Wesam','Almobaideen','wxacad','wxacad@rit.edu','D205','2050','MonWed: 11:00am - 12:30pm, TueThu: 1:30pm - 3:00pm');
+
+
+INSERT INTO course_genie.student (student_id, first_name, last_name, email) 
+VALUES
+  ('396001477','Jeeda','Kotob','jk8835@rit.edu'),
+  ('396001466','Ryma','Ait Tayeb','ra8245@rit.edu'),
+  ('396001488','Ibraheem','Mustafa','iam6172@rit.edu'),
+  ('396078872','Ayesha','Khan','ak8591@rit.edu');
+
+INSERT INTO course_genie.course (course_id, code, name, description, credits, discipline, course_approval_date, last_revision_date, undergraduate, new_course, course_deletion, graduate, prerequisites, corequisites, designated_innovation_journey_course)
+VALUES
+  (1,'ISTE 230','Introduction to Database and Data Modeling','The course provides foundational knowledge in database concepts, including the process of data modeling, the principles of the relational database model, normalization, and relational algebra.',3,'ISTE','Fall 2013','June 2019',1,0,0,0,'ISTE-120 or ISTE-200 or CSCI-140 or CSCI-142 or equivalent course.', 'None', 0),
+  (2,'GCIS 123','Software Development and Problem Solving I','This course focuses on algorithmic thinking, computational problem-solving, and applying these concepts to design and implement software solutions. Key topics include algorithmic thinking, object-oriented programming in Python, fundamental software engineering principles, and team-based work with distributed version control. ',4,'GCIS','Fall 2013','June 2024',1,0,0,0,'None', 'None', 0),
+  (3,'NSSA 102','Computer System Concepts','This course teaches the student the essential technologies needed by NSSA majors, focused on PC and mainframe hardware topics. They include how those platforms operate, how they are configured, and the operation of their major internal components. ',3,'NSSA','Fall 2013','May 2019',1,0,0,0, 'None', 'None', 0),
+  (4,'GCIS 124','Software Development and Problem Solving II','A second course that delves further into computational problem solving, now with a focus on an object-oriented perspective. There is a continued emphasis on basic software design, testing & verification, and incremental development',4,'GCIS','June 2020','Dec 2022',1,0,0,0, 'C- or better in GCIS-123 or CSEC-123 or ISTE-123 or equivalent course.', 'None', 0),
+  (5,'ISTE 140','Web & Mobile I','This course provides students with an introduction to Internet and Web technologies, and to development on Macintosh/UNIX computer platforms. opics include HTML and CSS, CSS3 features, digital images, Web page design and Web site publishing.',3,'ISTE','Fall 2013','July 2019',1,0,0,0, 'None', 'None', 0),
+  (6,'NSSA 241','Introduction to Routing & Switching','This course provides an introduction to wired network infrastructures, topologies, technologies, and the protocols required for effective end-to-end communication. Basic security concepts for TCP/IP based technologies are introduced.',3,'NSSA','Fall 2013','July 2022',1,0,0,0, 'NSSA-102 or CSEC-101 or CACT-151 or CSCI-250', 'None', 0),
+  (7,'NSSA 220','Task Automation with Interpreted Languages','An introduction to the Unix operating system and scripting in the Perl and Unix shell languages. The course will cover basic user-level commands to the Unix operating system, followed by basic control structures, and data structures in Perl.',3,'NSSA','Unlisted','August 2022',1,0,0,0, 'GCIS-124', 'None', 0),
+  (8,'ISTE 240','Web & Mobile II','This course builds on the basics of web page development that are presented in ISTE 140 and extends that knowledge to focus on theories, issues, and technologies related to the design and development of web sites.',3,'ISTE','Fall 2013','July 2019',1,0,0,0, 'ISTE-140', 'None', 0),
+  (9,'NSSA 221','System Administration I','This course is designed to give students an understanding of the role of the system administrator in large organizations. This will be accomplished through a discussion of many of the tasks and tools of system administration.',3,'NSSA','Fall 2013','July 2019',1,0,0,0, '(NSSA-220 or CSCI-141) and NSSA-241', 'None', 1),
+  (10,'SWEN 261','Introduction to Software Engineering','An introductory course in software engineering, emphasizing the organizational aspects of software development and software design and implementation by individuals and small teams within a process/product framework.',3,'SWEN','June 2022','June 2022',1,0,0,0, 'CSCI-140 or CSCI-142 or CSCI-242 or SWEN-124 or CSEC-124 or GCIS-124 or equivalent course.', 'None', 0),
+  (11,'ISTE 260','Designing the User Experience','The user experience is an important design element in the development of interactive systems. This course presents the foundations of user-centered design principles within the context of human-computer interaction (HCI).',3, 'ISTE','Fall 2013','July 2019',1,0,0,0, 'ISTE-140 or IGME-230 or NACA-172 or equivalent course.', 'None', 0),
+  (12,'ISTE 470','Data Mining & Exploration','Rapidly expanding volumes of data from all areas of society are becoming available in digital form. High value information and knowledge is embedded in many of these data volumes.',3,'ISTE','Feb 2018','August 2020',1,0,0,0, '(STAT-145 OR MATH- 251) and (GCIS-124 OR ISTE-121 OR CSCI- 140 OR CSCI-142 OR CSCI-242 OR ISTE-200 OR IGME-201 OR IGME-106) or equivalent courses', 'None', 0),
+  (13,'ISTE 330','Database Conectivity & Access','In this course, students will build applications that interact with databases. Through programming exercises, students will work with multiple databases and programmatically invoke the advanced database processing operations that are integral to contemporary computing applications. ',3,'ISTE','Fall 2013','July 2019',1,0,0,0, '(ISTE-230 or CSCI-320) and (ISTE-120 or GCIS-124 or CSCI-140 or CSCI-142 or CSCI-242) or equivalent courses.', 'None', 0),
+  (14,'ISTE 430','Information Requirements Modeling','Students will survey and apply contemporary techniques used in analyzing and modeling information requirements. Requirements will be elicited in a variety of domains and abstracted at conceptual, logical, and physical levels of detail.',3,'ISTE','Fall 2013','July 2019',1,0,0,0, 'ISTE-230 or CSCI-320 or equivalent course', 'None', 0),
+  (15,'NSSA 245','Network Services','This course will investigate the protocols used to support network-based services and the tasks involved in configuring and administering those services in virtualized Linux and Windows internetworking environments.',3,'NSSA','Fall 2013','Dec 2023',1,0,0,0, 'NSSA-241 and (NSSA-220 or CSCI- 141 or GCIS-123) and NSSA-221 or equivalent courses', 'None', 1),
+  (16,'NSSA 443','Network Design & Performance','This course will examine the design and performance of networks. Students will learn to design networks based on identified needs and analyze the performance of that network.',3,'NSSA','Fall 2013','May 2020',1,0,0,0, 'NSSA-241', 'None', 1),
+  (17,'ISTE 340','Client Programming','This course will explore the analysis, design, development, and implementation of client-side programming in the context of Internet technologies, mobile devices, Web-based client systems and desktop applications.',3,'ISTE','August 2024','October 2024',1,0,0,0, '(ISTE-240 or IGME-330) and (GCIS-124 or ISTE-121 or ISTE-200 or CSCI-142 or CSCI- 140 or IGME-106 or IGME-102 or GCIS-127) or equivalent courses.', 'None', 0),
+  (18,'ISTE 500','Senior Development Project I','The first course in a two-course, senior level, system development capstone project. Students form project teams and work with sponsors to define system requirements.',3,'ISTE','Fall 2013','July 2019',1,0,0,0, 'The completion of 2 co-ops is required', 'None', 1),
+  (19,'IGME 101','New Media Interactive Design and Algorithmic Problem Solving I','This course provides students with an introduction to problem solving, abstraction, and algorithmic thinking that is relevant across the field of new media.',4,'IGME','Fall 2013','July 2019',1,0,0,0, 'None', 'None', 0),
+  (20,'CSEC 201','Programming for Information Security','This course builds upon basic programming skills to give students the programming knowledge necessary to study computing security. Students will be introduced to network programming, memory management, and operating system calls along with associated security concepts.',3,'CSEC','Fall 2013','July 2019',1,0,0,0, 'CSEC-101 or CSEC-140) and (CSCI-142 or GCIS-124 or GCIS-127 or CSCI-242) or equivalent courses.', 'None', 0),
+  (21,'CSEC 140','Introduction to Cybersecurity','The course will teach students to think about information systems using an adversarial mindset, evaluate risk to information systems, and introduce controls that can be implemented to reduce risk.' ,3,'CSEC','Fall 2013','July 2019',1,0,0,0, 'None', 'None', 0);
+
+
+INSERT INTO course_genie.section (section_id, code, term, class_number, schedule, room, teaching_methodology, configured, course_id, professor_id)
+VALUES
+  -- Dr. Ali Assi (1)
+  (1,'602','Fall 2025','17638','MW 1:05 - 3:55','D207','In Person',1,2,1), -- GCIS 123
+  (2,'600','Fall 2025','17791','TR 3:00 - 4:20','H106','In Person',1,1,1), -- ISTE 230
+  (3,'602','Fall 2025','17928','TR 9:00 - 10:20','D113','In Person',1,1,1), -- ISTE 230
+  (4,'600','Fall 2025','17703','TR 1:30 - 2:50','D313','In Person',1,10,1), -- ISTE 230
+
+  -- Dr. Ahmed Mostafa (2)
+  (5,'606','Fall 2025','17840','MW 3:00 - 5:50','D313','In Person',1,2,2), -- GCIS 123
+  (6,'602','Fall 2025','17929','MWF 10:05 - 11:00','D113','In Person',1,11,2), -- ISTE 260
+  (7,'600','Fall 2025','18309','TR 9:00 - 11:50','H002','In Person',1,19,2), -- ISTE 260
+
+  -- Dr. Huda Saadeh (3)
+  (8,'600','Fall 2025','17482','TR 10:30 - 11:50','D112','In Person',1,20,3), -- CSEC 201
+  (9,'601','Fall 2025','17698','MW 1:05 - 2:25','D112','In Person',1,20,3), -- CSEC 201
+  (10,'602','Fall 2025','17838','MWF 11:10 - 12:05','D114','In Person',1,20,3), -- CSEC 201
+  (11,'602','Fall 2025','17838','TR 4:30 - 5:50','E104','In Person',1,18,3), -- ISTE 500
+
+  -- Dr. Qusai Hasan (4)
+  (12,'600','Fall 2025','17561','MW 1:05 - 2:25','D107','In Person',1,21,4), -- CSEC 140
+  (13,'601','Fall 2025','17568','MWF 11:10 - 12:05','D113','In Person',1,21,4), -- CSEC 140
+  (14,'602','Fall 2025','17752','TR 9:00 - 10:20','D107','In Person',1,21,4), -- CSEC 140
+  (15,'603','Fall 2025','17839','MWF 10:05 - 11:00','H212','In Person',1,21,4), -- CSEC 140
+  (16,'604','Fall 2025','17941','TR 4:30 - 5:50','E103','In Person',1,21,4), -- CSEC 140
+  (17,'603','Fall 2025','17895','MW 2:35 - 4:25','H106','In Person',1,3,4); -- NSSA 102
+ 
+
+INSERT INTO course_genie.clo (clo_id, name, description, course_id)
+VALUES 
+  (1,'Understand basic programming concepts','CLO1',1);
+
+INSERT INTO course_genie.assessment (assessment_id, name, category_name, short_name, week, max_points, topic,section_id)
+VALUES
+  (1,'Midterm Exam','Exam','Midterm',7,100,1,1);
+
+INSERT INTO course_genie.benchmark (benchmark_id, benchmark_type, description, threshold, percentage)
+VALUES
+  (1,'Grade','90% of students achieve grade >= 70%',70,90);
+
+INSERT INTO course_genie.assessment_clo (assessment_id, clo_id) 
+VALUES 
+  (1,1);
+
+INSERT INTO course_genie.book (id, isbn, title, url) 
+VALUES
+  (1,'9780306406157','Fundamentals of Database Systems','https://www.auhd.edu.ye/upfiles/elibrary/Azal2020-01-22-12-28-11-76901.pdf'),
+  (2,'9780306406658','Logic and Computer Design Fundamentals','https://www.auhd.edu.ye/upfiles/elibrary/Azal2020-01-22-12-28-11-76901.pdf');
+
+
+INSERT INTO course_genie.book_section (id, required_reading, suggested_reading, book_id, section_id)
+VALUES 
+  (1,1,1,1,1),
+  (2,1,1,1,2),
+  (3,1,1,1,3),
+  (4,1,1,1,4),
+  (5,1,1,1,5),
+  (6,1,1,1,6),
+  (7,1,1,1,7),
+  (8,1,1,1,8),
+  (9,1,1,1,9),
+  (10,1,1,1,10),
+  (11,1,1,1,11),
+  (12,1,1,1,12),
+  (13,1,1,1,13),
+  (14,1,1,1,14),
+  (15,1,1,1,15),
+  (16,1,1,1,16),
+  (17,1,1,1,17),
+  
+  (18,1,1,2,1),
+  (19,1,1,2,2),
+  (20,1,1,2,3),
+  (21,1,1,2,4),
+  (22,1,1,2,5),
+  (23,1,1,2,6),
+  (24,1,1,2,7),
+  (25,1,1,2,8),
+  (26,1,1,2,9),
+  (27,1,1,2,10),
+  (28,1,1,2,11),
+  (29,1,1,2,12),
+  (30,1,1,2,13),
+  (31,1,1,2,14),
+  (32,1,1,2,15),
+  (33,1,1,2,16),
+  (34,1,1,2,17);
+  
+
+-- INSERT INTO course_genie.syllabus (syllabus_id, content, section_id) 
+-- VALUES
+--   (1,'<p><b>Course Code:</b> ISTE230 <br><b>Section Code:</b> ISTE230-01<br><b>Course Name:</b> Introduction to Database and Data Modeling <br><b>Description:</b> Introduction to Mathematics<br><b>Credits:</b> 4<br><b>Semester:</b> Spring 2024<br><b>Professor:</b> User Two<br><br><b>Assessments:</b><br>- Exam (Final)<br>  <b>Max Points:</b> 100<br>  <b>CLOs:</b><br><br>- Assignment (HW3)<br>  <b>Max Points:</b> 50<br>  <b>CLOs:</b><br><br></p>', 1);
+
+INSERT INTO course_genie.grade (grade_id, score, assessment_id, student_id)
+VALUES 
+  (1,100,1,'396001477'),
+  (2,90,1,'396001466'),
+  (3,75,1,'396001488'),
+  (4,75,1,'396078872');
+
+INSERT INTO course_genie.professor_roles (professor_id, role)
+VALUES 
+  (1, 'PROFESSOR'),
+  (2, 'PROFESSOR'),
+  (3, 'PROFESSOR'),
+  (4, 'PROFESSOR'),
+  (5, 'PROFESSOR'),
+  (6, 'PROFESSOR'),
+  (7, 'PROFESSOR');
+
+INSERT INTO course_genie.category_description (id, category_name, description, section_id)
+VALUES 
+  (1,'Exam','Exam description',1);
+
+
+-- | assessment             | done
+-- | assessment_clo         | done
+-- | benchmark              | done
+-- | book                   | done
+-- | book_section           | done
+-- | category_description   | done
+-- | clo                    | done
+-- | course                 | done
+-- | grade                  | done
+-- | professor              | done
+-- | professor_roles        | done
+-- | section                | done
+-- | student                | done
+-- | syllabus               | done
