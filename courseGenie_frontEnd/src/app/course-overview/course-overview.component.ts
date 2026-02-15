c664aa2308c5c2149f446470224315e5e4701363
IbrahiM-MustafA111 <iam6172@g.rit.edu>
2026-02-15 17:38:56 +0400

    Implement admin role of Adding Section UI card and modal skeleton

--- courseGenie_frontEnd/src/app/course-overview/course-overview.component.ts
+++ courseGenie_frontEnd/src/app/course-overview/course-overview.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseService } from '../services/course.service';
 import { Observable } from 'rxjs';
 import { Course, Section } from '../home/course.model';
 import { map, switchMap, distinctUntilChanged } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
-@Component({
+@Component({ 
   selector: 'app-course-overview',
   standalone: false,
   templateUrl: './course-overview.component.html',
@@ -15,14 +17,19 @@ export class CourseOverviewComponent implements OnInit {
 
   course$!: Observable<Course>;
   sections$!: Observable<Section[]>;
+  isAdmin = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private courseService: CourseService
+    private courseService: CourseService,
+    private authService: AuthService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit() {
+    this.isAdmin = this.authService.hasRole('ROLE_ADMIN');
+
     this.course$ = this.route.paramMap.pipe(
       map(pm => pm.get('courseCode')!),
       distinctUntilChanged(),
@@ -37,4 +44,23 @@ export class CourseOverviewComponent implements OnInit {
   navigateToSection(courseCode: string, sectionCode: string) {
     this.router.navigate([`/overview/${courseCode}/${sectionCode}`]);
   }
+  openAddSectionModal(content: TemplateRef<any>) {
+    if (!this.isAdmin || this.modalService.hasOpenModals()) return;
+  
+    this.modalService.open(content, {
+      centered: true,
+      backdrop: 'static',
+      keyboard: true
+    });
+  }
+  
+  closeModal() {
+    this.modalService.dismissAll();
+  }
+  
+  onSectionCreateSubmit() {
+    // US6 scope: open/close popup only
+    this.closeModal();
+  }
+  
 }
